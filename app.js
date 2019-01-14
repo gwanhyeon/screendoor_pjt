72e6934b7b5439be3f4def667bc0656baf911678
Your Name <you@example.com>
2019-01-15 00:00:13 +0900

    node module

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var http = require('http');
 
 var server = http.createServer(function (req, res) {
+    console.log("서버열려라")
   res.writeHead(200, { 'Content-Type' : 'text/plain' });
   res.end('Hello World');
 });
