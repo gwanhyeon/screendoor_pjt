b7d1496329de4ed7be1a93c354f7d2453a316bb9
Your Name <you@example.com>
2019-01-15 21:05:27 +0900

    회원가입 업데이트

--- router/signup.js
+++ router/signup.js
@@ -1,43 +1,53 @@
 const express = require('express')
 const router = express.Router();
 const bodyParser = require('body-parser');
-const user = require('../models/user.js');
-var jsonParser = bodyParser.json();
-var urlencodeParser = bodyParser.urlEncodeParser({extended: false});
+const User = require('../models/user.js');
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
-router.get('/signup',(req,res) =>{
+router.get('/',(req,res) =>{
   res.writeHead('200',{'Content-Type':'text/html;charset=utf8'});
   res.write("<h1>로그인</h1>");
-  res.write("<form action='/signin' method='POST'>");
+  res.write("<form action='/signup' method='POST'>");
   res.write("<input name='user_id' type='text'/>");
   res.write("<input name='user_password' type='password'/>");
-  res.write("<input type='submit' value='signin'/>");
+  res.write("<input type='submit' value='signup'/>");
   res.write("</form>");
   
 })
 
-router.post('/signup',(req,res) =>{
+router.post('/',urlencodedParser,(req,res) =>{
   console.log(req.body);
-  var user_id = req.body.user_id
-  var user_password = req.body.user_password
+//   var users = new user();
+//   console.log(user.user_id);
+//   console.log(req.body.user_id);
+//   console.log(user.user_password);
+//   console.log(req.body.user_password);
 
-  user.insert({ user_id: user_id, user_password:user_password}, (err, user) => {
+  var user = new User({
+      "user_id" : req.body.user_id ,
+      "user_password" :req.body.user_password,
+      "user_name":req.body.name,
+      "user_age": req.body.age,
+      "user_git_id" : req.body.user_git_id});
+  user.save(function(err){
     if(err) {
+        res.send({status: "403",user_id: req.body.user_id});
+        console.log(req.body.user_id);
         throw err;
     }
+    res.send({status: "200",user_id:req.body.user_id});
+    console.log(user);
     
-    if(user.length > 0){
-        res.send({status: "200",user_id:user_id});
-        console.log(user);
-    }else{
-        res.send({status: "403",user_id:user_id});
-    }
+        // res.send({status: "403",user_id:user_id});
+        // console.log(req.body.user_id);
+    
  });
   
   
 })
 
 
-module.exports = rotuer;
+module.exports = router;
 
