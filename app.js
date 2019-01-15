a85ad4489560d1427421728f3172d5309bc0cf1b
Your Name <you@example.com>
2019-01-15 21:06:32 +0900

    App.js 업데이트

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express')
 const db = require('./database/db.js'); // db 불러오기
-
 const app = express();
-
 const signup = require('./router/signup.js');
+const signin = require('./router/signin.js');
+
 app.use('/signup',signup)
+app.use('/signin',signin)
+
 
 app.listen(3001, function(){
 	console.log('http://localhost:3001 connected');
