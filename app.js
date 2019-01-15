4d85daf697c5cb7473f28078118c1872039a4d14
Your Name <you@example.com>
2019-01-15 19:44:41 +0900

    버리기

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const db = require('./database/db.js'); // db 불러오기
 
 const app = express();
 
-const signin = require('./router/signin.js');
-app.use('/signin',signin)
+const signup = require('./router/signup.js');
+app.use('/signup',signup)
 
 app.listen(3001, function(){
 	console.log('http://localhost:3001 connected');
--- router/signup.js
+++ router/signup.js
@@ -1,13 +1,43 @@
 const express = require('express')
 const router = express.Router();
-const user = require('../models/user');
-router.get('sign_up',function(req,res,next){
-    res.render('user/signup')
-});
+const bodyParser = require('body-parser');
+const user = require('../models/user.js');
+var jsonParser = bodyParser.json();
+var urlencodeParser = bodyParser.urlEncodeParser({extended: false});
 
-router.post('/sign_up',function(req,res,next){
-    res.send(`<h1>Sign up</h1>`);
 
+router.get('/signup',(req,res) =>{
+  res.writeHead('200',{'Content-Type':'text/html;charset=utf8'});
+  res.write("<h1>로그인</h1>");
+  res.write("<form action='/signin' method='POST'>");
+  res.write("<input name='user_id' type='text'/>");
+  res.write("<input name='user_password' type='password'/>");
+  res.write("<input type='submit' value='signin'/>");
+  res.write("</form>");
+  
 })
 
-// export 
+router.post('/signup',(req,res) =>{
+  console.log(req.body);
+  var user_id = req.body.user_id
+  var user_password = req.body.user_password
+
+  user.insert({ user_id: user_id, user_password:user_password}, (err, user) => {
+    if(err) {
+        throw err;
+    }
+    
+    if(user.length > 0){
+        res.send({status: "200",user_id:user_id});
+        console.log(user);
+    }else{
+        res.send({status: "403",user_id:user_id});
+    }
+ });
+  
+  
+})
+
+
+module.exports = rotuer;
+
