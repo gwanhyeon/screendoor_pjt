99f8596d6a77cec79b6a09084097c874aad619f9
Rekt77 <timtaeil@gmail.com>
2019-01-16 04:37:20 +0900

    get 요청부분 삭제

--- screendoor_node/router/signin.js
+++ screendoor_node/router/signin.js
@@ -4,18 +4,6 @@ const bodyParser = require('body-parser');
 var passport = require('passport');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-router.get('/', (req, res) => {
-
-    res.writeHead('200',{'Content-Type':'text/html;charset=utf8'});
-    res.write("<h1>로그인</h1>");
-    res.write("<form action='/signin' method='POST'>");
-    res.write("<input name='user_id' type='text'/>");
-    res.write("<input name='user_password' type='password'/>");
-    res.write("<input type='submit' value='signin'/>");
-    res.write("</form>");
-
-  });
-
 router.post('/',urlencodedParser,(req,res) => {
   passport.authenticate('signin', (err,user,info) =>{
     var error = err||info
--- screendoor_node/router/signup.js
+++ screendoor_node/router/signup.js
@@ -4,18 +4,6 @@ const bodyParser = require('body-parser');
 var passport = require('passport');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-router.get('/',(req,res)=>{
-
-  res.writeHead('200',{'Content-Type':'text/html;charset=utf8'});
-  res.write("<h1>회원가입 페이지</h1>");
-  res.write("<form action='/signup' method='POST'>");
-  res.write("<input type='text' name='user_id'/>");
-  res.write("<input type='text' name='user_password'/>");
-  res.write("<input type='submit' value='signin'/>");
-  res.write("</form>");
-  
-})
-
 router.post('/',urlencodedParser,(req,res) => {
   passport.authenticate('signup', (err,user,info) =>{
     var error = err||info
