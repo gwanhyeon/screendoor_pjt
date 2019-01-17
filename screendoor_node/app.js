30eca0eff0b1f59911b487b39c259bde7c2a365e
kgh940525 <kgh940525@gmail.com>
2019-01-17 14:18:20 +0900

    오류수정

--- screendoor_node/app.js
+++ screendoor_node/app.js
@@ -1,32 +1,26 @@
 var express = require('express')
-// const session = require('express-session');
+const session = require('express-session');
 const db = require('./database/db.js'); // db 불러오기
 const app = express();
 const passport = require('passport');
 const passportConfig = require('./passports/localPassport');
 const signup = require('./router/signup.js');
 const signin = require('./router/signin.js');
-const authtest = require('./router/authtest.js');
 var flash = require('connect-flash');
-const jwtconfig = require('./jwt/config')
-
-// app.use(session({
-// 	secret:'secret-key',
-// 	resave:true,
-// 	saveUninitialized:false
-// }));
+const bodyParser = require('body-Parser')
+app.use(session({
+	secret:'secret-key',
+	resave:true,
+	saveUninitialized:false
+}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
-app.set('jwt-secret',jwtconfig.secret)
-
+app.use(bodyParser.json())
+app.use('/api/signin',signin)
+app.use('/api/signup',signup)
 db();
 passportConfig(passport);
-
-app.use('/api/signup',signup)
-app.use('/api/signin',signin)
-app.use('/authtest',authtest)
-
-app.listen(3002, ()=>{
+app.listen(3001, function(){
 	console.log('http://localhost:3001 connected');
 });
