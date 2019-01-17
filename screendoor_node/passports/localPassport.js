74a7acffa11df64e0b2f1235214f8d86fae6aec4
Rekt77 <timtaeil@gmail.com>
2019-01-17 19:00:33 +0900

    signup 입력값 변경 및 비구조화 할당 적용

--- screendoor_node/passports/localPassport.js
+++ screendoor_node/passports/localPassport.js
@@ -18,7 +18,7 @@ module.exports = (passport) => {
     passwordField: 'user_password',
     passReqToCallback: true,
   }, (req,user_id, user_password, done) => {
-    User.find({ user_id: req.body.user_id, user_password:req.body.user_password }, (err, user) => {
+    User.find({ user_id: user_id, user_password:user_password }, (err, user) => {
       if (err) return done(err); // 서버 에러 처리
       if (!user) return done(null, false, req.flash('signinMessage','login failure')); // 임의 에러 처리
       else{
@@ -36,16 +36,17 @@ module.exports = (passport) => {
     passwordField: 'user_password',
     passReqToCallback: true,},
     (req,user_id, user_password, done) => {
+    const { user_name, user_age, user_git_id} =req.body
     User.findOne({"user_id":user_id}, (err,user) => {
         if(err) return done(err);
         if(user) return done(null,false,req.flash('signupMessage','이메일이 존재합니다.'))
         else{
             var newUser = new User({
-                "user_id" : req.body.user_id ,
-                "user_password" :req.body.user_password,
-                "user_name":req.body.name,
-                "user_age": req.body.age,
-                "user_git_id" : req.body.user_git_id});
+                "user_id" : user_id ,
+                "user_password" : user_password,
+                "user_name": user_name,
+                "user_age": user_age,
+                "user_git_id" : user_git_id});
             newUser.save((err)=>{
                 if(err)
                     throw err;
