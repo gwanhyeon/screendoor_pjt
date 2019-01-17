977f06de7d8b1ab7abc2c3eb9c9ea5ae0f6cab35
kgh940525 <kgh940525@gmail.com>
2019-01-17 22:39:03 +0900

    concurrently module and proxy 3002 port setting

--- screendoor_node/router/signup.js
+++ screendoor_node/router/signup.js
@@ -8,10 +8,10 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 router.post('/',urlencodedParser,(req,res) => {
   passport.authenticate('signup', (err,user,info) =>{
     var error = err||info
-    if(error) return res.send({ status: 502, error:error });
-    if(!user) return res.send({ status: 401, message:'ID already exist'})
+    if(error) return res.json({ status: 502, error:error });
+    if(!user) return res.json({ status: 401, message:'ID already exist'})
     else{
-      return res.send({status:"200",user:req.body.user_id});
+      return res.json({status:200,user:req.body.user_id});
     }
   }
   )(req,res)
