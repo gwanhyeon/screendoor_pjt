8f6db6d795db87a17f38a75d40ebab09ff0ff769
Rekt77 <timtaeil@gmail.com>
2019-01-20 05:07:49 +0900

    SigninModal create

--- screendoor_react/src/components/NavigationBar.js
+++ screendoor_react/src/components/NavigationBar.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import logo from '../images/logo.png'
+import SigninModal from './SigninModal'
 import * as Gotham from '../fontStyle.css'
+
 
 const NavigationBar = () => {
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-fixed-top">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <div className="container">
             <a style={Gotham} className="navbar-brand" href="#"><img src={logo} width="30" height="30" alt="screen door"/>SCREEN DOOR</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,8 +23,10 @@ const NavigationBar = () => {
             </div>
             <form className="form-inline">
                 <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>{' '}
             </form>
+            &nbsp;
+            <SigninModal/>
             </div>
         </nav>
     )
