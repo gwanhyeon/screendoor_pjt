bcda1e3a1698fa46a6d6992398149191fc202503
kgh940525 <kgh940525@gmail.com>
2019-01-28 13:30:15 +0900

    Navigation -> MainComponent 변경, Navigationbar.js 생성,부트스트랩 scrollpy 적용(Home,About,Board)

---
+++ screendoor_react/src/components/About.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from '../images/logo.png'
+class About extends Component {
+    render() {
+        return (
+            <div>
+                About
+                <img src={logo}></img>
+                <br></br>
+                <img src={logo}></img>
+                <br></br>
+                <img src={logo}></img>
+                <br></br>
+                <img src={logo}></img>
+                
+
+            </div>
+        );
+    }
+}
+
+export default About;
